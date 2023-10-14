6c4f3323089310f2774d2c41919e0e4f91eb0e08
JJ Macalinao <i@jmac.ph>
2023-10-14 18:55:19 +0000

    Change to JSON configuration

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import datetime
+import json
 import os
 import time
 from onvif import ONVIFCamera
@@ -30,21 +31,23 @@ def set_time(tz: str, host: str, port: int, username: str, password: str):
     camera.devicemgmt.SetSystemDateAndTime(params)
 
 # Prep vars
-username = os.getenv('USER')
-password = os.getenv('PASS')
-camera_hosts = os.getenv('HOSTS').split(',') if 'HOSTS' in os.environ else None
+config = json.load(open(os.getenv('CONFIG_FILE') if 'CONFIG_FILE' in os.environ else 'config.json'))
+secrets = json.load(open(os.getenv('SECRETS_FILE') if 'SECRETS_FILE' in os.environ else 'secrets.json'))
 tz = os.getenv('POSIX_TZ') if 'POSIX_TZ' in os.environ else 'GMT'
 interval = float(os.getenv('INTERVAL')) if 'INTERVAL' in os.environ else 60
 
 # Main loop
 while True:
     try:
-        for str_host in camera_hosts:
-            host = str_host
-            port = 80
-            if ':' in str_host:
-                host = str_host.split(':')[0]
-                port = int(str_host.split(':')[1])
+        for camera in config['cameras']:
+            host = camera['host']
+            port = int(camera['port']) if 'port' in camera else None
+            username = secrets['defaults']['username']
+            password = secrets['defaults']['password']
+            camera_secret = next(filter(lambda x: x['name'] == camera['name'], secrets['cameras'])) if 'cameras' in secrets else None
+            if camera_secret:
+                username = camera_secret['username']
+                password = camera_secret['password']
             set_time(tz, host, port, username, password)
     except Exception as error:
         print('You dun goofed: ', error)
