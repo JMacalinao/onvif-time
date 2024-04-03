df3bc772dd9b99c38ac5279ddf0390dfe17f9c60
JJ Macalinao <i@jmac.ph>
2024-04-04 03:27:35 +0800

    Try-except for each camera

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+"""
+onvif-time
+"""
 import datetime
 import json
 import os
 import time
 from onvif import ONVIFCamera
 from zeep.transports import Transport
+
 
 def set_time(tz: str, host: str, port: int, username: str, password: str):
     camera = ONVIFCamera(host, port, username, password, transport=Transport(operation_timeout=5))
@@ -30,29 +34,35 @@ def set_time(tz: str, host: str, port: int, username: str, password: str):
     print(params)
     camera.devicemgmt.SetSystemDateAndTime(params)
 
+
 # Prep vars
-config = json.load(open(os.getenv('CONFIG_FILE') if 'CONFIG_FILE' in os.environ else 'config.json'))
-secrets = json.load(open(os.getenv('SECRETS_FILE') if 'SECRETS_FILE' in os.environ else 'secrets.json'))
+with open(os.getenv('CONFIG_FILE') if 'CONFIG_FILE' in os.environ else 'config.json',
+          "r", encoding="utf8") as file:
+    config = json.load(file.read())
+with open(os.getenv('SECRETS_FILE') if 'SECRETS_FILE' in os.environ else 'secrets.json',
+          "r", encoding="utf8") as file:
+    secrets = json.load(file.read())
 tz = os.getenv('POSIX_TZ') if 'POSIX_TZ' in os.environ else 'GMT'
 interval = float(os.getenv('INTERVAL')) if 'INTERVAL' in os.environ else 60
 
 # Main loop
 while True:
-    try:
-        for camera in config['cameras']:
+    for camera in config['cameras']:
+        try:
             host = camera['host']
             port = int(camera['port']) if 'port' in camera else None
-            username = None
-            password = None
+            username = "random"
+            password = "random"
             if 'defaults' in secrets:
                 defaults = secrets['defaults']
                 username = defaults['username'] if 'username' in defaults else None
                 password = defaults['password'] if 'password' in defaults else None
-            camera_secret = next(filter(lambda x: x['name'] == camera['name'], secrets['cameras'])) if 'cameras' in secrets else None
+            camera_secret = next(filter(lambda x: x['name'] == camera['name'],
+                                        secrets['cameras'])) if 'cameras' in secrets else None
             if camera_secret:
                 username = camera_secret['username']
                 password = camera_secret['password']
             set_time(tz, host, port, username, password)
-    except Exception as error:
-        print('You dun goofed: ', error)
+        except Exception as error:
+            print('You dun goofed: ', error)
     time.sleep(interval)
